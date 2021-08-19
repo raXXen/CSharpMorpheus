f955078d6e26c8cb4d17ecc0ab71a517e6367956
bjarn <bjarn@BJARNE>
2021-08-19 21:24:15 +0200

    Projektdateien hinzufügen.

---
+++ Klasse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMorpheus
+{
+    class Klasse
+    {
+        public int Zahl;
+        private String Zeichenkette;
+
+        //Konstructor
+        public Klasse(int n=1, string s="hello")
+        {
+            Zahl = n;
+            Zeichenkette = s;
+
+
+        }
+
+        public void gehen(int richtung = 1)
+        {
+            if (richtung == 1)
+            {
+                Zahl++;
+                if (Zahl < 1)
+                {
+                    Console.WriteLine("No Movement detected");
+                }
+            } 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMorpheus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aus("Hello",42);
+            Console.WriteLine(Program.fak(5));
+            Console.ReadKey();
+
+            Klasse uni = new Klasse(); //Mit dem Konstruktor aus Klasse.cs
+        }
+
+        //Rekursiv, Methode ruft sich selbst innerhalb der Methode auf
+        public static Int64 fak(int n)
+        {
+            if (n == 2)
+            {
+                return 2;
+            }
+            return n * fak(n - 1);
+        }
+        
+        //Überladung: zwei funktionen mit dem gleichen Namen. Der Compiler erkennt anhand der Parameter welche gemeint is. Siehe insbesondere Zeile 33.
+        public static void aus(string i, int j=1) //mit j=1 setzt 1 als Standartparameter für J es sei denn beim Aufrufen der Methode wird ein anderer Wert festgelegt bspw. 42 :)
+        {
+            for (int k=0; k < j; k++)
+            {
+                Program.aus(i);
+            }
+        }
+        public static void aus(string i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
